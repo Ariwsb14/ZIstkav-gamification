cec02a67e4b3fb39b1986e88fabf30a4f94538f9
Ariwsb14 <Ariasb1385@gmail.com>
2025-03-26 11:04:24 +0330

    signle letter views
    +single letter view and url

--- core/game/urls.py
+++ core/game/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'game'
 urlpatterns = [
-   path('',views.LettersView.as_view(), name='letters'),
+   path('letters/',views.LettersView.as_view(), name='letters'),
+   path('letters/<int:pk>/',views.SingleLetterView.as_view(), name='single_letter'),
 ]
--- core/game/views.py
+++ core/game/views.py
@@ -22,6 +22,31 @@ class LettersView(View):
                 else:
                     letter = letter +line + '\n'
             all_letters.append(letter)
-                
+        all_letters.reverse()         
         context = {'all_letters': all_letters, 'number_of_letters': number_of_letters}
         return render(request, 'game/letters.html', context)
+
+
+class SingleLetterView(View):
+
+    def get(self,request,pk):
+        user = request.user
+        all_letters = []
+        with open(f'md/{user.email}/letter.md', 'r') as file:
+            letters = file.read()
+            number_of_letters = 0
+            letter = ''
+            for line in letters.split('\n'):
+                
+                if line.startswith('#'):
+                    number_of_letters +=1
+                    all_letters.append(letter)
+                    if number_of_letters == pk:
+                        letter_title = line[1:].strip()
+                    letter = ''
+                else:
+                    letter = letter +line + '\n'
+            all_letters.append(letter)          
+        
+        context = {'letter': all_letters[pk], 'pk': pk ,'letter_title': letter_title}
+        return render(request, 'game/single_letter.html', context)
